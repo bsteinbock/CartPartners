f3a6de08aef002cfe7fdff365987916858d88ec2
bsteinbock <bill.steinbock@icloud.com>
2026-03-31 22:47:10 -0400

    Refactor Layout component to include screen options for improved navigation

--- app/(tabs)/groups/_layout.tsx
+++ app/(tabs)/groups/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router';
 export default function Layout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerBackTitle: '', headerBackButtonDisplayMode: 'minimal' }}>
       <Stack.Screen name="index" options={{ title: 'Groups', headerShown: false }} />
     </Stack>
   );
